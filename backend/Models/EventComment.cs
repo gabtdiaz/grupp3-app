dfa59704e25080105f539ad031cd55c8010bcc84
gabtdiaz <gabriella.thorisson@gmail.com>
2026-01-20 21:24:30 +0100

    added new class -eventComment

---
+++ backend/Models/EventComment.cs
@@ -0,0 +1,19 @@
+namespace grupp3_app.Api.Models;
+
+public class EventComment
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } //Ska man kunna redigera kommentar?
+
+    // FK
+
+    public int UserId { get; set; }
+    public int EventId { get; set; }
+
+    // Navigation properties
+    public User User { get; set; } = null!;
+    public Event Event { get; set; } = null!; 
+}
+
