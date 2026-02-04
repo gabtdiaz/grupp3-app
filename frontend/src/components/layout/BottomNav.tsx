8cb239a52e171aca1488173013b4123266616bc3
Benjamin Burton <benjamin.alexander.burton@gmail.com>
2026-02-04 10:54:29 +0100

    Fixed navigation to profile page through navbar

--- frontend/src/components/layout/BottomNav.tsx
+++ frontend/src/components/layout/BottomNav.tsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export default function BottomNav() {
+  const navigate = useNavigate();
+
   return (
     <div
       className="flex h-full w-full bg-cover"
-      style={{
-        backgroundImage: "url('/footer-background.png')",
-      }}
+      style={{ backgroundImage: "url('/footer-background.png')" }}
     >
-      <button className="flex-1 flex items-center justify-center">
+      <button
+        className="flex-1 flex items-center justify-center"
+        onClick={() => navigate("/activity")}
+      >
         <img
           src="/icons/calendar-icon.svg"
           alt="Calendar"
@@ -14,15 +19,21 @@ export default function BottomNav() {
         />
       </button>
 
-      <button className="flex-1 flex items-center justify-center">
+      {/* <button
+        className="flex-1 flex items-center justify-center"
+        onClick={() => navigate("/activity")}
+      >
         <img
           src="/icons/notification-icon.svg"
           alt="Notifications"
           className="w-6 h-6"
         />
-      </button>
+      </button> */}
 
-      <button className="flex-1 flex items-center justify-center">
+      <button
+        className="flex-1 flex items-center justify-center"
+        onClick={() => navigate("/profile")}
+      >
         <img src="/icons/profile-icon.svg" alt="Profile" className="w-5 h-5" />
       </button>
     </div>
