a4a94785d6262463f931a5557654275cae815a7b
Filippannilsson <filippanilsson95@gmail.com>
2026-02-05 12:45:14 +0100

    New migration

---
+++ backend/Migrations/20260205114314_AddPrivacySettingsToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace grupp3_app.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrivacySettingsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
